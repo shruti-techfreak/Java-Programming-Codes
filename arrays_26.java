8be638843c1886897b127e1256d663e4ad8d2778
Shruti <107032060+shruti-techfreak@users.noreply.github.com>
2023-06-05 23:16:02 +0530

    Add files via upload

---
+++ arrays_26.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class arrays_26 {
+    public static void main(String[] args) {
+        int [] marks= new int[5];
+        marks[0]=100;
+        marks[1]=60;
+        marks[2]=70;
+        marks[3]=85;
+        marks[4]=90;
+        System.out.println(marks[3]);
+        int num[]= {56, 45,100, 87};
+        System.out.println(num[2]);
+    }
+}
