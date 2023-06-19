2dad68ce095b979cba4b9b879a0fb847b1c4cfb5
Shruti <107032060+shruti-techfreak@users.noreply.github.com>
2023-06-19 11:10:42 +0530

    Create Square fill pattern.java

---
+++ Pattern problems/Square fill pattern.java
@@ -0,0 +1,17 @@
+public class Patternproblems {
+
+	public static void main(String[] args)
+	{
+		int i, j;
+		int n =4 ;
+
+		for (i = 0; i <= n; i++) {
+
+			for (j = 0; j <= n; j++) {
+				System.out.print("* ");
+			}
+
+			System.out.println();
+		}
+	}
+}
